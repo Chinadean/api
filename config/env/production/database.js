f10bccdf10acee86ea65754ecbd1a3de1c0245cd
7alip <talipaltas@gmail.com>
2022-02-24 22:47:54 +0100

    Add database pool option

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,8 +8,16 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USER'),
         password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        pool: {
+          min: 0,
+          max: 48,
+          idleTimeoutMillis: 30000000,
+          createTimeoutMillis: 30000000,
+          acquireTimeoutMillis: 30000000,
+          propagateCreateError: false,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
       },
       debug: false,
