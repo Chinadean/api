690814b5a9cdbbfec0f4b3440e4a4df214a728c1
7alip <talipaltas@gmail.com>
2022-02-25 09:57:12 +0100

    Allow graphql production playground

--- config/plugins.js
+++ config/plugins.js
@@ -10,6 +10,18 @@ module.exports = ({ env }) => ({
       client,
     },
   },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
   email: {
     enabled: true,
     config: {
