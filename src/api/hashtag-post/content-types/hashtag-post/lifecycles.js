a643de6929ff6feb2382041f287ca493c2af13be
7alip <talipaltas@gmail.com>
2022-02-28 13:27:59 +0100

    Allow setting post translator manually

--- src/api/hashtag-post/content-types/hashtag-post/lifecycles.js
+++ src/api/hashtag-post/content-types/hashtag-post/lifecycles.js
@@ -2,7 +2,8 @@ module.exports = {
   beforeCreate(event) {
     const { data } = event.params
 
-    if (data.localizations[0]) data.translator = data.createdBy
-    else data.creator = data.createdBy
+    if (data.localizations[0]) {
+      if (!data.translator) data.translator = data.createdBy
+    } else data.creator = data.createdBy
   },
 }
