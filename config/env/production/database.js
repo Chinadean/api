726a38f51dcd717282baa5420760ca02dcfcc777
7alip <talipaltas@gmail.com>
2022-03-07 17:02:56 +0100

    Return back to postgres

---
+++ config/env/production/database.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => {
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: '5432',
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USER'),
+        password: env('DATABASE_PASSWORD'),
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000000,
+          createTimeoutMillis: 30000000,
+          acquireTimeoutMillis: 30000000,
+          propagateCreateError: false,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      },
+      debug: false,
+    },
+  }
+}
