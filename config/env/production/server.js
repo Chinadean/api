97734c73c1f62ec50783786b56abf29c4928408f
7alip <talipaltas@gmail.com>
2022-03-07 09:41:57 +0100

    Add app keys variables to server config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,9 @@ const cronTasks = require('./cron-tasks')
 module.exports = ({ env }) => ({
   host: '0.0.0.0',
   url: 'https://api.samenvvv.nl',
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
