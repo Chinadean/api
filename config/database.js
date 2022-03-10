a7140852956bae31990d22c6583caddce46e53f5
7alip <talipaltas@gmail.com>
2022-03-10 16:08:45 +0100

    Remove database config strings

--- config/database.js
+++ config/database.js
@@ -2,16 +2,16 @@ const { parse } = require('pg-connection-string')
 
 module.exports = ({ env }) => {
   const { host, user, password, database } = parse(env('DATABASE_URL'))
-  console.log('parse', parse(env('DATABASE_URL')))
+
   return {
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', host || 'localhost'),
+        host: env('DATABASE_HOST', host),
         port: '5432',
-        database: env('DATABASE_NAME', database || 'samen_api'),
-        user: env('DATABASE_USER', user || 'samen_api_user'),
-        password: env('DATABASE_PASSWORD', password || 'Samen123'),
+        database: env('DATABASE_NAME', database),
+        user: env('DATABASE_USER', user),
+        password: env('DATABASE_PASSWORD', password),
         pool: {
           min: 0,
           max: 10,
