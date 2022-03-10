63be11137b0daad9c85cfee652a3807461d556e8
7alip <talipaltas@gmail.com>
2022-03-10 15:10:33 +0100

    Add pg-connection-string

--- config/database.js
+++ config/database.js
@@ -1,15 +1,30 @@
-const path = require('path')
+const { parse } = require('pg-connection-string')
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  const { host, user, password, database } = parse(env('DATABASE_URL'))
+  console.log('parse', parse(env('DATABASE_URL')))
+  return {
     connection: {
-      filename: path.join(
-        __dirname,
-        '..',
-        env('DATABASE_FILENAME', '.tmp/data.db'),
-      ),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', host || 'localhost'),
+        port: '5432',
+        database: env('DATABASE_NAME', database || 'samen_api'),
+        user: env('DATABASE_USER', user || 'samen_api_user'),
+        password: env('DATABASE_PASSWORD', password || 'Samen123'),
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000000,
+          createTimeoutMillis: 30000000,
+          acquireTimeoutMillis: 30000000,
+          propagateCreateError: false,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-})
+  }
+}
