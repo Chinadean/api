9e179cc97799a5eabe04aed7f3262e8305da9014
7alip <talipaltas@gmail.com>
2022-03-15 23:12:53 +0100

    Fix beforeCreaty lifecycle hook relation query

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -5,7 +5,7 @@ module.exports = {
     if (data.localizations && data.localizations[0]) {
       const translator = await strapi.db
         .query('api::translator.translator')
-        .findOne({ where: { user: { id: data.createdBy } } })
+        .findOne({ where: { volunteer: { user: { id: data.createdBy } } } })
 
       if (!data.translator) data.translator = translator.id
     } else data.creator = data.createdBy
