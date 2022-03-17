ec2710628d1b2c87a0070b738655e2413770e70c
7alip <talipaltas@gmail.com>
2022-03-17 03:17:47 +0100

    Comment out posts lifecycle method

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -3,15 +3,13 @@ module.exports = {
     const { data } = event.params
 
     if (data.localizations && data.localizations[0]) {
-      const translator = await strapi.db
-        .query('api::translator.translator')
-        .findOne({
-          where: { volunteer: { user: { id: data.createdBy } } },
-        })
-
-      if (!translator) throw Error('You must add translator')
-
-      if (!data.translator) data.translator = translator.id
+      //   const translator = await strapi.db
+      //     .query('api::translator.translator')
+      //     .findOne({
+      //       where: { volunteer: { user: { id: data.createdBy } } },
+      //     })
+      // if (!translator) throw Error('You must add translator')
+      // if (!data.translator) data.translator = translator.id
     } else data.creator = data.createdBy
   },
 }
