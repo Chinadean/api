f60021760141dad3fab1e749a44e1561d77ca590
7alip <talipaltas@gmail.com>
2022-05-12 21:29:38 +0200

    Add token salt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'e9a13e487ddd361c5fba80040173f01f'),
   },
